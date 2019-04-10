8822bb76fadf9241f89ebb1d5e256bb87a3e53f3
Henrique Cardoso - HC <henriquefleuryc@gmail.com>
2019-04-10 14:07:22 -0300

    Flake8 Style Guide Enforcement

--- sockmerchant.py
+++ sockmerchant.py
@@ -1,26 +1,18 @@
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the sockMerchant function below.
 def sockMerchant(n, ar):
-    
+
     numberPair = 0
     uniquelist = []
-    
+
     for color1 in ar:
         if color1 not in uniquelist:
             uniquelist.append(color1)
         else:
-            numberPair+=1
+            numberPair += 1
             uniquelist.remove(color1)
-        
+
     return numberPair
 
 
-
 n = 9
-ar = [10,20,20,10,10,30,50,10,20]
+ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 print(sockMerchant(n, ar))
